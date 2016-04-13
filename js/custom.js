b3d82d5dcd9890739b10c3ca2f8920fa9afaf9c4
Taj <tajdar.afridi123@gmail.com>
2016-04-13 13:19:05 +0500

    Go to top link is added

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,22 @@ $(document).ready(function(){
 	$(".advance-btn").click(function(){
       $("#advance-search-container").slideToggle();
     });
+	$(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+
 });
