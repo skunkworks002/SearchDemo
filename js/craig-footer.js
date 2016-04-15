1443b4001f2f8f6c6bb32fb5cee8e3f14b0edfae
skunkworks002 <skunkworks002@gmail.com>
2016-04-15 17:47:17 +0500

    test
    test

--- js/craig-footer.js
+++ js/craig-footer.js
@@ -8,7 +8,7 @@ if(jQuery.isEmptyObject(QueryString) === false) {
 	for(var t in QueryString) {
 		if(t == "q") $("#search_string").val(QueryString[t]);
 		if(t == "zipcode") {
-			console.log(jQuery.isEmptyObject(QueryString));
+			console.log(QueryString.length);
 			$("#location").val(QueryString[t]);
 			if(jQuery.isEmptyObject(QueryString) === true) {
 				$(".result").html('<p>No Result found</p>');
